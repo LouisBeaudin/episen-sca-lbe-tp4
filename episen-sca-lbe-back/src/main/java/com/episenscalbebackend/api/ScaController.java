3894c1da888f40cdeee29e97945ef95de11957d5
LouisBeaudin <louisbeaudin@outlook.fr>
2020-11-19 16:11:36 +0100

    ajout du logger

--- episen-sca-lbe-back/src/main/java/com/episenscalbebackend/api/ScaController.java
+++ episen-sca-lbe-back/src/main/java/com/episenscalbebackend/api/ScaController.java
@@ -1,6 +1,7 @@
 package com.episenscalbebackend.api;
 
 import com.episenscalbebackend.util.PrintWrite;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ScaController {
 
+    private static Logger logger = Logger.getLogger(ScaController.class);
+
     @PostMapping("/add/order")
     public String scalabilitepost(@RequestBody String order) {
-        System.out.println("on est la");
+        logger.info(order);
         PrintWrite.write(order);
         return "ok";
     }
